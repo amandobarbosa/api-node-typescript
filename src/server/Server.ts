901db2636a8018adc24dddee7c37d6ab399bc5ce
amandobarbosa <amandobarbosa7@gmail.com>
2025-05-15 21:47:42 -0300

    add .env

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { router } from "./routes";
+import 'dotenv/config'
 
 const server = express();
 
-server.get("/", (_, res) => {
-  return res.send("Olá dev!");
-});
+server.use(express.json())
+server.use(router)
+
 export { server };
