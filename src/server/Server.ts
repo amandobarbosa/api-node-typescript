e492eef338215638539e3c8a9f8f24380fef02fd
amandobarbosa <amandobarbosa7@gmail.com>
2025-05-17 12:06:53 -0300

    validando todos os erros

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,11 @@
+import "./shared/services/TranslationsYup";
 import express from "express";
 import { router } from "./routes";
-import 'dotenv/config'
+import "dotenv/config";
 
 const server = express();
 
-server.use(express.json())
-server.use(router)
+server.use(express.json());
+server.use(router);
 
 export { server };
--- src/server/controlers/cidades/create.ts
+++ src/server/controlers/cidades/create.ts
@@ -4,22 +4,30 @@ import * as Yup from "yup";
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 const bodyValidation: Yup.ObjectSchema<ICidade> = Yup.object().shape({
   nome: Yup.string().required().min(3),
+  estado: Yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validateData: ICidade | undefined = undefined;
   try {
-    validateData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as Yup.ValidationError;
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    validateData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
     });
+  } catch (err) {
+    const yupError = err as Yup.ValidationError;
+
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   console.log(validateData);
