035f0ceaee466b0df3df0e934d1af560ab2199e8
amandobarbosa <amandobarbosa7@gmail.com>
2025-07-14 19:26:27 -0300

    adicionando autenticação nas rotas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { CidadesController, PessoasController, UsuariosController } from "../controlers";
+import {
+  CidadesController,
+  PessoasController,
+  UsuariosController,
+} from "../controlers";
+import { ensureAuthenticated } from "../shared/middlewares";
 
 const router = Router();
 
@@ -8,52 +13,62 @@ router.get("/", (_, res) => {
 });
 router.post(
   "/cidades",
+  ensureAuthenticated,
   CidadesController.createValidation,
   CidadesController.create
 );
 router.get(
   "/cidades",
+  ensureAuthenticated,
   CidadesController.getAllValidation,
   CidadesController.getAll
 );
 router.get(
   "/cidades/:id",
+  ensureAuthenticated,
   CidadesController.getByIdValidation,
   CidadesController.getById
 );
 router.put(
   "/cidades/:id",
+  ensureAuthenticated,
   CidadesController.UpdateByIdValidation,
   CidadesController.updateById
 );
 router.delete(
   "/cidades/:id",
+  ensureAuthenticated,
   CidadesController.DeleteByIdValidation,
   CidadesController.DeleteById
 );
 
 router.get(
   "/pessoas",
+  ensureAuthenticated,
   PessoasController.getAllValidation,
   PessoasController.getAll
 );
 router.post(
   "/pessoas",
+  ensureAuthenticated,
   PessoasController.createValidation,
   PessoasController.create
 );
 router.get(
   "/pessoas/:id",
+  ensureAuthenticated,
   PessoasController.getByIdValidation,
   PessoasController.getById
 );
 router.put(
   "/pessoas/:id",
+  ensureAuthenticated,
   PessoasController.UpdateByIdValidation,
   PessoasController.updateById
 );
 router.delete(
   "/pessoas/:id",
+  ensureAuthenticated,
   PessoasController.DeleteByIdValidation,
   PessoasController.DeleteById
 );
